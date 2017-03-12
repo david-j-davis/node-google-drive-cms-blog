92c89bc1ddfcd55b020d6d24db2d5ec5a0eb9929
David Davis <davidjamesdavis.djd@gmail.com>
2017-03-12 12:16:24 -0700

    blog posts retrieval from firebase

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,20 @@ var got = require('got');
 const FirebaseREST = require('firebase-rest').default;
 var jsonClient = new FirebaseREST.JSONClient('https://node-heroku-blog.firebaseio.com/', { auth: '8OicTSxmbUQ0Da7wdWTN9dkJZDZOp3G865U6LxkH' });
 
-jsonClient.get('/').then(console.log);
+var blogData;
+
+jsonClient.get('/')
+  .then(res => blogData = res.body);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-
-
-    // got('todomvc.com')
-    //     .then(response => {
-    //         console.log(response.body);
-    //         //=> '<!doctype html> ...'
-    //     })
-    //     .catch(error => {
-    //         console.log(error.response.body);
-    //         //=> 'Internal server error ...'
-    //     });
-
     req.somedata = 'some information ... ';
-    console.log('app is: ', req.app);
     next();
 },function(req, res) {
     //console.log(req.identifier);
-    console.log(req.somedata);
-    res.render('index', { title: 'Express', name: 'David' });
+    //console.log(req.somedata);
+    res.render('index', { title: 'Express', name: 'David', posts: blogData});
 });
 
 module.exports = router;
