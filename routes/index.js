9c95fe06e855f13777efdfb44df88e43a430fd9a
David Davis <davidjamesdavis.djd@gmail.com>
2017-03-12 12:51:24 -0700

    updated posts pug

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/', function (req, res, next) {
 },function(req, res) {
     //console.log(req.identifier);
     //console.log(req.somedata);
-    res.render('index', { title: 'Example Blog Using Google Drive CMS, Firebase, and a node/express backend', name: 'David', posts: blogData});
+    res.render('index', { title: 'Example Blog Using Google Drive CMS, Firebase, and a node/express backend', name: 'David Davis', posts: blogData});
 });
 
 module.exports = router;
